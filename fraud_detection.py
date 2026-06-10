61e6fccd4877a02510ea70214e971b37a670a4de
Uppaluri Eswarachari <eswarchari789@gmail.com>
2026-06-10 11:36:35 +0530

    Add files via upload

--- fraud_detection.py
+++ fraud_detection.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 data = pd.read_csv("creditcard.csv")
 
+print("Dataset Loaded Successfully")
 print(data.head())
-
 print("Rows:", len(data))
+print("Columns:", len(data.columns))
